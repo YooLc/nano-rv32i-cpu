a66af2bc0997266943f28faf2afa3724296bea99
李晨潇 <3220101835@zju.edu.cn>
2024-10-09 16:05:24 +0800

    [feat] Fill in ctrl, core and cmp

--- sources_1/core/HazardDetectionUnit.v
+++ sources_1/core/HazardDetectionUnit.v
@@ -1,15 +1,29 @@
-`timescale 1ps/1ps
+`timescale 1ps / 1ps
 
-module HazardDetectionUnit(
-    input clk,
-    input Branch_ID, rs1use_ID, rs2use_ID,
-    input[1:0] hazard_optype_ID,
-    input[4:0] rd_EXE, rd_MEM, rs1_ID, rs2_ID, rs2_EXE,
-    output PC_EN_IF, reg_FD_EN, reg_FD_stall, reg_FD_flush,
-        reg_DE_EN, reg_DE_flush, reg_EM_EN, reg_EM_flush, reg_MW_EN,
-    output forward_ctrl_ls,
-    output[1:0] forward_ctrl_A, forward_ctrl_B
+module HazardDetectionUnit (
+    input wire clk,
+    input wire Branch_ID,
+    input wire rs1use_ID,
+    input wire rs2use_ID,
+    input wire [1:0] hazard_optype_ID,
+    input wire [4:0] rd_EXE,
+    input wire [4:0] rd_MEM,
+    input wire [4:0] rs1_ID,
+    input wire [4:0] rs2_ID,
+    input wire [4:0] rs2_EXE,
+    output wire PC_EN_IF,
+    output wire reg_FD_EN,
+    output wire reg_FD_stall,
+    output wire reg_FD_flush,
+    output wire reg_DE_EN,
+    output wire reg_DE_flush,
+    output wire reg_EM_EN,
+    output wire reg_EM_flush,
+    output wire reg_MW_EN,
+    output wire forward_ctrl_ls,
+    output wire [1:0] forward_ctrl_A,
+    output wire [1:0] forward_ctrl_B
 );
-    //according to the diagram, design the Hazard Detection Unit
+  //according to the diagram, design the Hazard Detection Unit
 
 endmodule
